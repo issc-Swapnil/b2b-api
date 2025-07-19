dbcda729ceb8e1bf253a3c4045c358a74c4c95b1
issc-Swapnil <patilswapnil8956@gmail.com>
2025-07-19 12:43:10 +0530

    fixed key

--- api/routes/NewsRoutes.js
+++ api/routes/NewsRoutes.js
@@ -1,29 +1,22 @@
 require('dotenv').config();
 const express = require('express');
-const NewsAPI = require('newsapi');
 const router = express.Router();
 
 const part1 = "0fe12b422";
 const part2 = "fc34a63a8";
 const part3 = "3386e94d";
-const part4 = process.env.NEWS_APIKEY || "434e57";
+const part4 = "434e57";
 
-function getKey() {
-  return part1 + part2 + part3 + part4;
-}
-
-const newsapi = new NewsAPI(getKey());
 
 router.get('/b2b-news', async (req, res) => {
   try {
-    const response = await newsapi.v2.everything({
-      q: 'B2B Lead Generation',
-      language: 'en',
-      pageSize: 10
-    });
+    const url = `https://newsapi.org/v2/everything?q=B2B%20Lead%20Generation&language=en&pageSize=10&apiKey=${part1 + part2 + part3 + part4}`;
+
+    const response = await axios.get(url);
+
     res.json({
       message: 'News fetched successfully',
-      data: response.articles
+      data: response.data.articles
     });
   } catch (err) {
     res.status(500).json({ error: err.message });
