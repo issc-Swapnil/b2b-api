a8cce897011e4ffa12667639e16921cd28890805
issc-Swapnil <patilswapnil8956@gmail.com>
2025-07-23 13:07:33 +0530

    fix api key

--- api/routes/NewsRoutes.js
+++ api/routes/NewsRoutes.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 
 router.get('/b2b-news', async (req, res) => {
   try {
-    const url = `https://newsdata.io/api/1/latest?apikey=${API_KEY}&q=B2B%20Lead%20Generation%2C%20B2B%20Advertising`;
+    const url = `https://newsdata.io/api/1/latest?apikey=${process.env.API_KEY}&q=B2B%20Lead%20Generation%2C%20B2B%20Advertising`;
 
     const response = await axios.get(url);
 
